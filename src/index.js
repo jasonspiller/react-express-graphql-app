64d821b37b236a745f1ed99dd77d378d61c84b78
Jason Spiller <jason@thetargetlab.com>
2018-11-15 13:00:27 -0700

    Adding authentications.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { Security, ImplicitCallback } from '@okta/okta-react';
 import { ApolloProvider } from 'react-apollo';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -8,11 +10,21 @@ import * as serviceWorker from './serviceWorker';
 import client from './apollo';
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
+  <BrowserRouter>
+    <Security
+      issuer={`${process.env.REACT_APP_OKTA_ORG_URL}/oauth2/default`}
+      redirect_uri={`${window.location.origin}/implicit/callback`}
+      client_id={process.env.REACT_APP_OKTA_CLIENT_ID}
+    >
+      <ApolloProvider client={client}>
+        <Route path="/implicit/callback" component={ImplicitCallback} />
+        <Route path="/" component={App} />
+      </ApolloProvider>
+    </Security>
+  </BrowserRouter>,
   document.getElementById('root')
 );
+
 serviceWorker.unregister();
 
 if (module.hot) {
